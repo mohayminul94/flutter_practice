f675da4e043a2d52cb06ef4b564145b41c33f699
Mohayminul_Islam <mohayminul02@gmail.com>
2025-02-01 11:59:04 +0600

    MediaQuery Wrap, LayoutBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,127 +11,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.pinkAccent,
-          foregroundColor: Colors.white,
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("MyApp"),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // Name Field
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    labelStyle: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    hintText: "Enter your name",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8), // Rounded corners
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    hintStyle: TextStyle(
-                      color: Colors.blue.shade500,
-                    ),
-                    prefixIcon: const Icon(Icons.person), // Better icon
-                    suffixIcon: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.search),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
+      home: Home(),
+    );
+  }
+}
 
-                // Description Field
-                TextField(
-                  maxLines: 5,
-                  maxLength: 500,
-                  onTap: () {},
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    alignLabelWithHint: true, // Align label properly
-                    labelStyle: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    hintText: "Enter your description",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    hintStyle: TextStyle(
-                      color: Colors.blue.shade500,
-                    ),
-                  ),
-                ),
-                TextField(
-                  maxLines: 5,
-                  maxLength: 500,
-                  onTap: () {},
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    alignLabelWithHint: true, // Align label properly
-                    labelStyle: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    hintText: "Enter your description",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    hintStyle: TextStyle(
-                      color: Colors.blue.shade500,
-                    ),
-                  ),
-                ),
-                TextField(
-                  maxLines: 5,
-                  maxLength: 500,
-                  onTap: () {},
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    alignLabelWithHint: true, // Align label properly
-                    labelStyle: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    hintText: "Enter your description",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: const BorderSide(color: Colors.blue, width: 2),
-                    ),
-                    hintStyle: TextStyle(
-                      color: Colors.blue.shade500,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // --------------------MediaQuery ------------------------
+
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+
+    print(mediaQuery.size.width);
+
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Column(
+        children: [
+          Wrap(
+            alignment: WrapAlignment.center,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              Text('Name : MD Mohayminul Islam Rayhan '),
+              Text('University : DIU'),
+              Text('Class Roll : 11')
+            ],
+          )
+        ],
       ),
     );
   }
 }
+
